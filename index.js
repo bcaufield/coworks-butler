7a5e9ec7305501179469db4f6e6b6696e7aca30a
Brian Caufield <brian.caufield@gmail.com>
2015-10-04 15:09:56 -0400

    debugging check plan

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ app.use(express.static(__dirname + '/pub'));
 // Splash page
 app.get('/splash', function(req, res) {
   logger.wireless('Splashdown', {session: req.session});
-  logger.wireless('req.session.node_mac', req.session.node_mac);
+  //logger.wireless('req.session.node_mac', req.session.node_mac);
   if (serverConfig.authType == 'allow-all') {
     res.redirect(req.session.base_grant_url + '?continue_url=' + req.session.user_continue_url);
   } else {
@@ -205,7 +205,7 @@ app.post(apiSlug + '/auth', function(req, res) {
   var user = req.body.username,
     pass = req.body.password;
 
-  logger.wireless('index.js /auth', user, pass, JSON.stringify(req.session));  
+  //logger.wireless('index.js /auth', user, pass, JSON.stringify(req.session));  
   if (user && pass) {
     var deny = function(message) {
       logger.wireless('Denied access to %s, Rejection: %s', user, message, {session: req.session})
@@ -228,7 +228,7 @@ app.post(apiSlug + '/auth', function(req, res) {
     nex.authUser(user, pass, function(result, message) {
       logger.wireless('authUser', result, message);
       if (result) {
-        logger.wireless('authUser, user = ', JSON.stringify(user));
+        //logger.wireless('authUser, user = ', JSON.stringify(user));
         
         // User is who they say they are, look them up
         nex.findUser(user, 'Coworker_Email', function(data, err) {
@@ -238,11 +238,11 @@ app.post(apiSlug + '/auth', function(req, res) {
           //if (!err && data && data.length === 1) {
           if (!err && data) {  
             var userInfo = data.pop();
-            logger.wireless('got user info = ', JSON.stringify(userInfo));
+            //logger.wireless('got user info = ', JSON.stringify(userInfo));
 
             if (userInfo.Id) {
               var nexUser = new NexudusUser(nex, userInfo.Id);
-              logger.wireless('user session = ', JSON.stringify(req.session));
+              //logger.wireless('user session = ', JSON.stringify(req.session));
               nexUser.isValid(req.session.client_mac, function(data, err) {
                 if (!err && data) {
                   nexUser.addMac(req.session.client_mac, function(data, err) {
